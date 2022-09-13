cd5cfc6b6eb14bb85d5f9bf401cc49b4edc60320
Murad <muradmustafayev03@gmail.com>
2022-09-13 23:45:13 +0400

    completed many local minima functions and added docstrings

--- benchmark_functions/many_local_minimums.py
+++ benchmark_functions/many_local_minimums.py
@@ -1,13 +1,31 @@
 from imports import *
 
 
-def ackley(x: Vector, a=20, b=0.2, c=2 * pi) -> float:
+def ackley(x: Vector, a=20, b=0.2, c=2*pi) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-32.768, 32.768], for all i = 1, ..., d \n
+        Recommended variable values are: a = 20, b = 0.2 and c = 2π \n
+        Global Minimum: f(x*) = 0 at x* = (0,...,0)
+    """
     d = len(x)
-    return -1 * a * exp(-1 * b * sqrt((1 / d) * np.sum(square(x)))) - exp(
+    return -1 * a * exp(-1 * b * sqrt((1 / d) * sum(square(x)))) - exp(
         (1 / d) * np.sum(cos(x * np.full(shape=d, fill_value=c)))) + a + exp(1)
 
 
+def griewank(x: Vector) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-600, 600], for all i = 1, ..., d \n
+        Global Minimum: f(x*) = 0 at x* = (0,...,0)
+    """
+    d = len(x)
+    return sum(x[i] ** 2 / 4000 for i in range(d)) - prod(cos(x[i] / sqrt(i)) for i in range(d)) + 1
+
+
 def levy(x: Vector) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-10, 10], for all i = 1, ..., d \n
+        Global Minimum: f(x*) = 0 at x* = (1,...,1)
+    """
     d = len(x)
     w = [1 + (i - 1) / 4 for i in x]
     return sin(pi * w[0]) ** 2 + \
@@ -16,14 +34,28 @@ def levy(x: Vector) -> float:
 
 
 def rastrigin(x: Vector) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-5.12, 5.12], for all i = 1, ..., d \n
+        Global Minimum: f(x*) = 0 at x* = (0,...,0)
+    """
     d = len(x)
     return 10 * d + sum(x[i] ** 2 - 10 * cos(2 * pi * x[i]) for i in range(d))
 
 
 def schwefel(x: Vector) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-500, 500], for all i = 1, ..., d \n
+        Global Minimum: f(x*) = 0 at x* = (420.9687,...,420.9687)
+    """
     d = len(x)
     return 418.9829 * d - sum(x[i] * sin(sqrt(abs(x[i]))) for i in range(d))
 
 
 def shubert(x: Vector) -> float:
+    """
+        The function is usually evaluated on the square xi ∈ [-10, 10], for all i = 1, 2, \n
+        although this may be restricted to the square xi ∈ [-5.12, 5.12], for all i = 1, 2 \n
+        Recommended passing x of length 2 (2-dimensional function) \n
+        Global Minimum when len(x) = 2: f(x*) = -186.7309
+    """
     return float(prod(sum(i * cos(i + 1) * x_j for i in range(1, 6)) for x_j in x))
