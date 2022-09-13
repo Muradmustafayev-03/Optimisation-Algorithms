94049b4b3b7dc64c8b5b58c75a5d3ff9cb96cdc2
Murad <muradmustafayev03@gmail.com>
2022-09-14 03:12:36 +0400

    dixon-price func

--- benchmark_functions/other.py
+++ benchmark_functions/other.py
@@ -1,10 +1,18 @@
 from imports import *
 
+def dixon_price(x: Vector) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-10, 10], for all i = 1,..., d \n
+         Global Minimum: f(x*) = 0 at x_i = 2^(-(2^i - 2)/2^i) for i = 1,...,d
+    """
+    d = len(x)
+    return (x[0] - 1) ** 2 + sum(((i+1) * 2 * x[i] ** 2 - x[i-1]) ** 2 for i in range(1, d))
+
 
 def rosenbrock(x: Vector) -> float:
     """
-        The function is usually evaluated on the hypercube xi ∈ [-5, 10], for all i = 1, …, d, \n,
-        although it may be restricted to the hypercube xi ∈ [-2.048, 2.048], for all i = 1, …, d \n
+        The function is usually evaluated on the hypercube xi ∈ [-5, 10], for all i = 1,..., d, \n,
+        although it may be restricted to the hypercube xi ∈ [-2.048, 2.048], for all i = 1,..., d \n
         Global Minimum: f(x*) = 0 at x* = (1,...,1)
     """
     d = len(x)
@@ -12,7 +20,11 @@ def rosenbrock(x: Vector) -> float:
 
 
 def zakharov(x: Vector) -> float:
+    """
+        The function is usually evaluated on the hypercube xi ∈ [-5, 10], for all i = 1,..., d \n
+        Global Minimum: f(x*) = 0 at x* = (0,...,0)
+    """
     d = len(x)
     return sum(x[i] ** 2 for i in range(d)) + \
-           sum(0.5 * (i+1) * x[i] for i in range(d)) ** 2 + \
-           sum(0.5 * (i+1) * x[i] for i in range(d)) ** 4
+           sum(0.5 * (i + 1) * x[i] for i in range(d)) ** 2 + \
+           sum(0.5 * (i + 1) * x[i] for i in range(d)) ** 4
