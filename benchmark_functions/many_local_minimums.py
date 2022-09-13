7867cae67f5604e35750a4fd3d7e3eb10c183ff7
Murad <muradmustafayev03@gmail.com>
2022-09-13 22:53:42 +0400

    ...

--- benchmark_functions/many_local_minimums.py
+++ benchmark_functions/many_local_minimums.py
@@ -1,13 +1,13 @@
 from imports import *
 
 
-def ackley(x: Vector, a=20, b=0.2, c=2 * pi) -> int:
+def ackley(x: Vector, a=20, b=0.2, c=2 * pi) -> float:
     d = len(x)
     return -1 * a * exp(-1 * b * sqrt((1 / d) * np.sum(square(x)))) - exp(
         (1 / d) * np.sum(cos(x * np.full(shape=d, fill_value=c)))) + a + exp(1)
 
 
-def levy(x: Vector) -> int:
+def levy(x: Vector) -> float:
     d = len(x)
     w = [1 + (i - 1) / 4 for i in x]
     return sin(pi * w[0]) ** 2 + \
@@ -15,15 +15,15 @@ def levy(x: Vector) -> int:
            (w[d-1] - 1) ** 2 * (1 + 10 * sin(2 * pi * w[d-1] + 1) ** 2)
 
 
-def rastrigin(x: Vector) -> int:
+def rastrigin(x: Vector) -> float:
     d = len(x)
     return 10 * d + sum(x[i] ** 2 - 10 * cos(2 * pi * x[i]) for i in range(d))
 
 
-def schwefel(x: Vector) -> int:
+def schwefel(x: Vector) -> float:
     d = len(x)
     return 418.9829 * d - sum(x[i] * sin(sqrt(abs(x[i]))) for i in range(d))
 
 
-def shubert(x: Vector) -> int:
-    return prod(sum(i * cos(i + 1) * x_j for i in range(1, 6)) for x_j in x)
+def shubert(x: Vector) -> float:
+    return float(prod(sum(i * cos(i + 1) * x_j for i in range(1, 6)) for x_j in x))
--- benchmark_functions/valey_shaped.py
+++ benchmark_functions/valey_shaped.py
@@ -1,5 +1,6 @@
 from imports import *
 
-def rosenbrock(x: Vector) -> int:
+
+def rosenbrock(x: Vector) -> float:
     d = len(x)
     return sum(100 * (x[i + 1] - x[i] ** 2) ** 2 + (x[i] - 1) ** 2 for i in range(d - 1))
