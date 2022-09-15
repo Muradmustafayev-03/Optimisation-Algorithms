9b3f0416aec04f9627cc8607262ae625ee0f97df
Murad <muradmustafayev03@gmail.com>
2022-09-15 15:39:35 +0400

    ...

--- benchmark_functions/gradients.py
+++ benchmark_functions/gradients.py
@@ -1,33 +1,34 @@
 from .imports import *
 
 
-def perm_gradient(x: Vector, beta=10) -> float:
+def perm_gradient(x: Vector, beta=10) -> Vector:
     pass
 
 
-def perm0_gradient(x: Vector, beta=10) -> float:
+def perm0_gradient(x: Vector, beta=10) -> Vector:
+    d = len(x)
+
+
+
+def rosenbrock_gradient(x: Vector) -> Vector:
     pass
 
 
-def rosenbrock_gradient(x: Vector) -> float:
+def rotated_hyper_ellipsoid_gradient(x: Vector) -> Vector:
     pass
 
 
-def rotated_hyper_ellipsoid_gradient(x: Vector) -> float:
+def sphere_gradient(x: Vector) -> Vector:
     pass
 
 
-def sphere_gradient(x: Vector) -> float:
+def sum_of_powers_gradient(x: Vector) -> Vector:
     pass
 
 
-def sum_of_powers_gradient(x: Vector) -> float:
+def sum_of_squares_gradient(x: Vector) -> Vector:
     pass
 
 
-def sum_of_squares_gradient(x: Vector) -> float:
-    pass
-
-
-def trid(x: Vector) -> float:
+def trid(x: Vector) -> Vector:
     pass
