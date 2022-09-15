cdff1cabc7d0b711404babbfea8fa0d7c0959a64
Murad <muradmustafayev03@gmail.com>
2022-09-15 13:58:02 +0400

    gradient descent

---
+++ iterative_algorithms/gradient_descent.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def gradient_descent(gradient, d, alpha=0.2, tc_value=10 ** (-20), max_iterations=1000000, _range=1000000):
+    current = (np.random.rand(d) - 0.5) * _range
+
+    for _ in range(max_iterations):
+        step = alpha * gradient(current)
+
+        if step.all() < tc_value:
+            break
+
+        current -= step
+
+    return current
