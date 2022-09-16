339e5db21ff80969f5fce16a2ffe945f6b052368
Murad <muradmustafayev03@gmail.com>
2022-09-17 00:40:38 +0400

    ...

--- tests.py
+++ tests.py
@@ -5,5 +5,5 @@ from benchmark_functions.gradients import sphere_gradient, sum_of_powers_gradien
 print(gradient_descent(sphere_gradient, 10))
 print(gradient_descent(sum_of_powers_gradient, 4, _range=3))
 print(gradient_descent(sum_of_squares_gradient, 4, _range=20))
-print(gradient_descent(trid_gradient, 5, max_iterations=10, alpha=0.3, _range=50))
+print(gradient_descent(trid_gradient, 5, _range=50))
 
