6c73cff9f9b15bc04d39712f2fc352067494318d
Murad <muradmustafayev03@gmail.com>
2022-09-25 14:07:51 +0400

    ...

--- iterative_algorithms/GradientDescent.py
+++ iterative_algorithms/GradientDescent.py
@@ -14,7 +14,7 @@ class GradientDescent:
     def grad(self, x: np.array) -> np.array:
         pass
 
-    def optimize(self, max_iterations, alpha: float = 0.2, tol: float = 10 ** (-20)) -> np.array:
+    def optimize(self, max_iterations: int = 10000, alpha: float = 0.2, tol: float = 10 ** (-20)) -> np.array:
         current = (np.random.rand(self.d) - 0.5) * self.range
 
         for _ in range(max_iterations):
@@ -47,49 +47,5 @@ class ApproximatedGradientDescent(GradientDescent):
         new[new == init] = place
         return new
 
-    def grad(self, x: np.array, delta: float = 10 ** (-20)) -> np.array:
+    def grad(self, x: np.array, delta: float = 10 ** (-10)) -> np.array:
         return np.array([(self.func(self.replace(x, x_i, x_i + delta)) - self.func(x)) / delta for x_i in x])
-
-
-#   ######################TO#DELETE#ALL#BELOW###############################
-
-def grad(func, x, delta=10 ** (-20)):
-    def replace(array, init, place):
-        new = np.copy(array)
-        new[new == init] = place
-        return new
-
-    return np.array([(func(replace(x, x_i, x_i + delta)) - func(x)) / delta for x_i in x])
-
-
-def batch_gradient_descent(gradient: callable, d: int, alpha: float = 0.2, tol: float = 10 ** (-20),
-                           randomize: bool = False, max_iterations: int = 1000000, _range: int = 1000):
-    try:
-        current = (np.random.rand(d) - 0.5) * _range
-
-        for _ in range(max_iterations):
-            step = alpha * gradient(current) * (random.random() ** int(randomize))
-
-            if step.all() < tol:
-                break
-            current = current - step
-
-        return current
-
-    except RuntimeWarning:
-        return batch_gradient_descent(gradient, d, alpha, tol, randomize, max_iterations, _range)
-
-
-def approximated_gradient_descent(func: callable, d: int, alpha: float = 0.2, tol: float = 10 ** (-20),
-                                  grad_delta: float = 10 ** (-8), randomize: bool = False,
-                                  max_iterations: int = 1000000, _range: int = 1000):
-    current = (np.random.rand(d) - 0.5) * _range
-
-    for _ in range(max_iterations):
-        step = alpha * grad(func, current, grad_delta) * (random.random() ** int(randomize))
-
-        if step.all() < tol:
-            break
-        current = current - step
-
-    return current
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from iterative_algorithms.GradientDescent import batch_gradient_descent, approximated_gradient_descent
+from iterative_algorithms.GradientDescent import BatchGradientDescent, ApproximatedGradientDescent
 from metaheuristic_algorithms.HarmonySearch import HarmonySearch
 from metaheuristic_algorithms.GeneticAlgorithm import GeneticAlgorithm
 from benchmark_functions.many_local_minimums import *
@@ -7,19 +7,19 @@ from benchmark_functions.gradients import *
 
 
 def test_batch_gradient_descent():
-    print(batch_gradient_descent(rotated_hyper_ellipsoid_gradient, 4, _range=130))  # works up to d=4
-    print(batch_gradient_descent(sphere_gradient, 18))  # works perfectly with any parameters
-    print(batch_gradient_descent(sum_of_powers_gradient, 4, _range=2))  # works well up to d=4, then precision falls
-    print(batch_gradient_descent(sum_of_squares_gradient, 4, _range=20))  # works up to d=4
-    print(batch_gradient_descent(trid_gradient, 10, _range=200))  # works perfectly with any parameters
+    print(BatchGradientDescent(rotated_hyper_ellipsoid_gradient, 4, _range=130).optimize())  # works up to d=4
+    print(BatchGradientDescent(sphere_gradient, 18).optimize())  # works perfectly with any parameters
+    print(BatchGradientDescent(sum_of_powers_gradient, 4, _range=2).optimize())  # works well up to d=4, then worse
+    print(BatchGradientDescent(sum_of_squares_gradient, 4, _range=20).optimize())  # works up to d=4
+    print(BatchGradientDescent(trid_gradient, 10, _range=200).optimize())  # works perfectly with any parameters
 
 
 def test_approximated_gradient_descent():
-    print(approximated_gradient_descent(rotated_hyper_ellipsoid, 4, _range=130))  # works up to d=4
-    print(approximated_gradient_descent(sphere, 18))  # works perfectly with any parameters
-    print(approximated_gradient_descent(sum_of_powers, 2, _range=2))  # works well up to d=1, then precision falls
-    print(approximated_gradient_descent(sum_of_squares, 4, _range=20))  # works up to d=4
-    print(approximated_gradient_descent(trid, 10, _range=200))  # works well with any parameters
+    print(ApproximatedGradientDescent(rotated_hyper_ellipsoid, 4, _range=130).optimize())  # works up to d=4
+    print(ApproximatedGradientDescent(sphere, 18).optimize())  # works perfectly with any parameters
+    print(ApproximatedGradientDescent(sum_of_powers, 2, _range=2).optimize())  # works well up to d=2, then worse
+    print(ApproximatedGradientDescent(sum_of_squares, 4, _range=20).optimize())  # works up to d=4
+    print(ApproximatedGradientDescent(trid, 10, _range=200).optimize())  # works well with any parameters
 
 
 def test_hs():
