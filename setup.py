e9dd38c192f5f530a77730aed204dc5fcd86220a
Murad <muradmustafayev03@gmail.com>
2022-09-26 04:42:09 +0400

    setup

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="optimisation-algorithms",  # Required
+    name="Optimisation-Algorithms",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
