7c806e5634a4b5887ff4fa9f43b3e3fe0ecb3847
Murad <muradmustafayev03@gmail.com>
2023-02-15 14:46:12 +0400

    Gradient Descent

--- src/optimisation_algorithms/Algorithmic/GradientDescent.py
+++ src/optimisation_algorithms/Algorithmic/GradientDescent.py
@@ -1,177 +1,96 @@
-import random
-from abc import abstractmethod
 import numpy as np
-from .exceptions.FailedToConverge import FailedToConverge
 
 
 class GradientDescent:
     """
-    Optimisation using Gradient Descent.
-
-    Attributes
-    ----------
-    d: int
-        Number of dimensions of the function [f(x) = f(x_1, ..., x_d)]
-    _range: float
-        Range of values for the initial population x_1 in (-range/2; range/2)
-
-    Methods
-    -------
-    grad(self, x: np.array) -> numpy.array
-        Abstract method, to be overridden in child classes to return gradient of the function.
-
-    optimize(self, max_iterations: int = 10000, alpha: float = 0.2, tol: float = 10 ** (-20),
-                 randomize: bool = False) -> numpy.array
-        Finds a local minima of the function.
-    :return:
+    Batch Gradient Descent.
     """
 
-    def __init__(self, d: int, _range: float = 1000):
-        """
-        Parameters
-        ----------
-        d: int
-            Number of dimensions of the function [f(x) = f(x_1, ..., x_d)]
-        _range: float
-            Range of values for the initial population x_1 in (-range/2; range/2)
-
-        :return: None
-        """
-        self.d = d
-        self.range = _range
-
-    @abstractmethod
-    def grad(self, x: np.array) -> np.array:
-        pass
-
-    def optimize(self, max_iterations: int = 100000, alpha: float = 0.02, tol: float = 10 ** (-20),
-                 randomize: bool = False) -> np.array:
-        """
-        Finds a local minima of the function.
-
-        Parameters
-        ----------
-        max_iterations: int
-            Maximal number of iterations
-        alpha: float
-            Descent change rate
-        tol: float
-            Tolerance number, maximal number to be considered as 0
-        randomize: bool
-            If True, randomizes change rate each step
-
-        Returns
-        -------
-        :return: numpy.array
-            x_min = (x_1, ..., x_d) where f(x_min) = min f(x)
-        """
-        current = (np.random.rand(self.d) - 0.5) * self.range
-
-        for _ in range(max_iterations):
-            try:
-                step = self.grad(current) * alpha * (random.random() ** int(randomize))
-
-                if step.all() < tol:
-                    break
-                current = current - step
-            except RuntimeWarning as w:
-                raise FailedToConverge().with_traceback(w.__traceback__)
-
-        return current
-
-
-class BatchGradientDescent(GradientDescent):
-    """
-    Optimisation using Batch Gradient Descent.
-
-    Attributes
-    ----------
-    gradient: callable
-        Gradient of the function
-    d: int
-        Number of dimensions of the function [f(x) = f(x_1, ..., x_d)]
-    _range: float
-        Range of values for the initial population x_1 in (-range/2; range/2)
-
-    Methods
-    -------
-    grad(self, x: np.array) -> numpy.array
-        Gradient of the function.
-
-    optimize(self, max_iterations: int = 10000, alpha: float = 0.2, tol: float = 10 ** (-20),
-                 randomize: bool = False) -> numpy.array
-        Inherited from the parent class. Finds a local minima of the function.
-    :return:
-    """
-
-    def __init__(self, gradient: callable, d: int, _range: float = 1000):
-        """
-        Parameters
-        ----------
-        gradient: callable
-            Gradient of the function
-        d: int
-            Number of dimensions of the function [f(x) = f(x_1, ..., x_d)]
-        _range: float
-            Range of values for the initial population x_1 in (-range/2; range/2)
-
-        :return: None
-        """
-        super().__init__(d, _range)
-        self.gradient = gradient
-
-    def grad(self, x: np.array) -> np.array:
-        return self.gradient(x)
-
-
-class ApproximatedGradientDescent(GradientDescent):
-    """
-    Optimisation using Batch Gradient Descent.
-
-    Attributes
-    ----------
-    function: callable
-        Function to minimize
-    d: int
-        Number of dimensions of the function [f(x) = f(x_1, ..., x_d)]
-    _range: float
-        Range of values for the initial population x_1 in (-range/2; range/2)
-
-    Methods
-    -------
-    replace(array, init, place)
-        Replaces an element in a numpy.array
-
-    grad(self, x: np.array) -> numpy.array
-        Gradient of the function.
-
-    optimize(self, max_iterations: int = 10000, alpha: float = 0.2, tol: float = 10 ** (-20),
-                 randomize: bool = False) -> numpy.array
-        Inherited from the parent class. Finds a local minima of the function.
-    :return:
-    """
-
-    def __init__(self, func: callable, d: int, _range: float = 1000):
-        """
-        Parameters
-        ----------
-        function: callable
-            Function to minimize
-        d: int
-            Number of dimensions of the function [f(x) = f(x_1, ..., x_d)]
-        _range: float
-            Range of values for the initial population x_1 in (-range/2; range/2)
-
-        :return: None
-        """
-        super().__init__(d, _range)
-        self.func = func
+    def __init__(self, learning_rate: float = 0.1, max_iter: int = 1000, tol: float = 1e-8):
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+        self.tol = tol
 
     @staticmethod
-    def replace(array, init, place):
-        new = np.copy(array)
-        new[new == init] = place
-        return new
+    def gradient(f, x, h=1e-8):
+        """
+        Computes the gradient of a function f at point x.
 
-    def grad(self, x: np.array, delta: float = 10 ** (-10)) -> np.array:
-        return np.array([(self.func(self.replace(x, x_i, x_i + delta)) - self.func(x)) / delta for x_i in x])
+        Parameters:
+        ----------
+        f : callable
+            A callable function.
+        x : numpy.ndarray
+            An array representing the point at which to compute the gradient.
+        h : float
+            A small value to use in the computation of the gradient.
+
+        Returns:
+        -------
+        numpy.ndarray:
+            An array representing the gradient of f at point x.
+        """
+
+        assert callable(f), "f must be a callable function"
+        assert isinstance(h, float) and h > 0, "h should be a positive float"
+        assert isinstance(x, np.ndarray), "x should be a NumPy array"
+
+        gradient = np.zeros_like(x)
+        identity = np.identity(len(x))
+        for i in range(len(x)):
+            gradient[i] = (f(x + h * identity[i]) - f(x - h * identity[i])) / (2 * h)
+        return gradient
+
+    def fit(self, f, d):
+        """
+        Finds the minimum of a function f using gradient descent starting from x0.
+        """
+        assert callable(f), "f must be a callable function"
+        assert isinstance(d, int) and d > 0, "d should be a positive integer"
+
+        x = np.random.rand(d)
+        for i in range(self.max_iter):
+            grad = self.gradient(f, x)
+            if np.linalg.norm(grad) < self.tol:
+                break
+            x -= self.learning_rate * grad
+        return x, f(x)
+
+
+class MiniBatchGD(GradientDescent):
+    """
+    Mini-batch Gradient Descent.
+    """
+
+    def __init__(self, batch_size=10, learning_rate=0.1, max_iter=1000, tol=1e-8):
+        super().__init__(learning_rate, max_iter, tol)
+        self.batch_size = batch_size
+
+    def fit(self, f, d):
+        """
+        Finds the minimum of a function f using mini-batch gradient descent starting from x0.
+        """
+        assert callable(f), "f must be a callable function"
+        assert isinstance(d, int) and d > 0, "d should be a positive integer"
+
+        x = np.random.rand(d)
+        f_old = f(x)
+        for i in range(self.max_iter):
+            indices = np.random.choice(d, self.batch_size)
+            grad = self.gradient(f, x[indices])
+            x[indices] -= self.learning_rate * grad
+            f_new = f(x)
+            if np.abs(f_new - f_old) < self.tol:
+                break
+            f_old = f_new
+
+        return x, f(x)
+
+
+class SGD(MiniBatchGD):
+    """
+    Stochastic Gradient Descent.
+    """
+
+    def __init__(self, learning_rate=0.1, max_iter=1000, tol=1e-8):
+        super().__init__(batch_size=1, learning_rate=learning_rate, max_iter=max_iter, tol=tol)
