cc4223f332927b9fd1e807b97e743b835fd56d20
Murad <muradmustafayev03@gmail.com>
2023-02-15 15:47:26 +0400

    cleaned up the code

--- src/optimisation_algorithms/Algorithmic/GradientDescent.py
+++ src/optimisation_algorithms/Algorithmic/GradientDescent.py
@@ -1,18 +1,35 @@
+from typing import Tuple
 import numpy as np
 
 
 class GradientDescent:
     """
     Batch Gradient Descent.
+
+    Attributes:
+    ----------
+    learning_rate : float
+        The learning rate to use in the gradient descent update.
+    tol : float
+        The convergence threshold for the norm of the gradient.
+    max_iter : int
+        The maximum number of iterations to run before stopping.
+
+    Methods:
+    -------
+    gradient(f, x, h=1e-8):
+        Computes the gradient of a function f at point x.
+    fit(f, d, maximize=False):
+        Finds the minimum or maximum of a function f using gradient descent starting from a random point.
     """
 
-    def __init__(self, learning_rate: float = 0.1, max_iter: int = 1000, tol: float = 1e-8):
+    def __init__(self, learning_rate: float = 0.1, max_iter: int = 1e4, tol: float = 1e-8):
         self.learning_rate = learning_rate
         self.max_iter = max_iter
         self.tol = tol
 
     @staticmethod
-    def gradient(f, x, h=1e-8):
+    def gradient(f: callable, x: np.ndarray, h=1e-8):
         """
         Computes the gradient of a function f at point x.
 
@@ -41,44 +58,89 @@ class GradientDescent:
             gradient[i] = (f(x + h * identity[i]) - f(x - h * identity[i])) / (2 * h)
         return gradient
 
-    def fit(self, f, d):
+    def fit(self, f, d, maximize=False) -> Tuple[np.ndarray, float]:
         """
-        Finds the minimum of a function f using gradient descent starting from x0.
+        Finds the minimum or maximum of a function f using mini-batch gradient descent starting from a random point.
+
+        Parameters
+        ----------
+        f : callable
+            A callable function.
+        d : int
+            A positive integer representing the number of features.
+        maximize : bool, optional
+            If True, the method will find the maximum of the function. Otherwise, the default is False, and the method
+            will find the minimum of the function.
+
+        Returns
+        -------
+        Tuple[numpy.ndarray, float]
+            The optimal point and the optimal value of f.
         """
+
         assert callable(f), "f must be a callable function"
         assert isinstance(d, int) and d > 0, "d should be a positive integer"
 
-        x = np.random.rand(d)
+        rng = np.random.default_rng()
+        x = rng.random(d)
+        sign = 1 if maximize else -1
         for i in range(self.max_iter):
             grad = self.gradient(f, x)
             if np.linalg.norm(grad) < self.tol:
                 break
-            x -= self.learning_rate * grad
+            x += sign * self.learning_rate * grad
         return x, f(x)
 
 
 class MiniBatchGD(GradientDescent):
     """
-    Mini-batch Gradient Descent.
+    Mini-batch gradient descent optimizer.
+
+    Parameters
+    ----------
+    batch_size : int, optional
+        The size of the mini-batch. Default is 10.
+    learning_rate : float, optional
+        The learning rate. Default is 0.1.
+    max_iter : int, optional
+        The maximum number of iterations. Default is 1000.
+    tol : float, optional
+        The tolerance for convergence. Default is 1e-8.
     """
 
-    def __init__(self, batch_size=10, learning_rate=0.1, max_iter=1000, tol=1e-8):
+    def __init__(self, batch_size=10, learning_rate=0.1, max_iter=1e4, tol=1e-8):
         super().__init__(learning_rate, max_iter, tol)
         self.batch_size = batch_size
 
-    def fit(self, f, d):
+    def fit(self, f: callable, d: int, maximize: bool = False) -> Tuple[np.ndarray, float]:
         """
-        Finds the minimum of a function f using mini-batch gradient descent starting from x0.
+        Finds the minimum or maximum of a function f using mini-batch gradient descent starting from a random point.
+
+        Parameters
+        ----------
+        f : callable
+            A callable function.
+        d : int
+            A positive integer representing the number of features.
+        maximize : bool, optional
+            If True, the method will find the maximum of the function. Otherwise, the default is False, and the method
+            will find the minimum of the function.
+
+        Returns
+        -------
+        Tuple[numpy.ndarray, float]
+            The optimal point and the optimal value of f.
         """
         assert callable(f), "f must be a callable function"
         assert isinstance(d, int) and d > 0, "d should be a positive integer"
 
         x = np.random.rand(d)
+        sign = -1 if maximize else 1
         f_old = f(x)
         for i in range(self.max_iter):
             indices = np.random.choice(d, self.batch_size)
             grad = self.gradient(f, x[indices])
-            x[indices] -= self.learning_rate * grad
+            x[indices] += sign * self.learning_rate * grad
             f_new = f(x)
             if np.abs(f_new - f_old) < self.tol:
                 break
