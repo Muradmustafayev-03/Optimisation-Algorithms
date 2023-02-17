e3706ffc22bc9996bbd1dac368308baf9539ed04
Murad <muradmustafayev03@gmail.com>
2023-02-17 22:43:45 +0400

    Conjugate Gradient Descent method

--- src/optimisation_algorithms/Algorithmic/GradientDescent.py
+++ src/optimisation_algorithms/Algorithmic/GradientDescent.py
@@ -1,5 +1,5 @@
-from .GradientDescentAbstract import BatchGD as Batch, StochasticGD as Stochastic, MiniBatchGD as MiniBatch
-from .GradientDescentAbstract import SimpleGD as Simple, ConjugateGD as Conjugate
+from GradientDescentAbstract import BatchGD as Batch, StochasticGD as Stochastic, MiniBatchGD as MiniBatch
+from GradientDescentAbstract import SimpleGD as Simple, ConjugateGD as Conjugate
 
 
 class GradientDescent(Simple, Batch):
@@ -21,3 +21,22 @@ class MiniBatchGradientDescent(Simple, MiniBatch):
         Simple.__init__(self, f, d, learning_rate, max_iter, tol, h, rand_min, rand_max)
         MiniBatch.__init__(self, d, batch_size, tol)
 
+
+class ConjugateGradientDescent(Conjugate, Batch):
+    def __init__(self, f: callable, d: int, max_iter: int = 10 ** 5, tol: float = 1e-8, h: float = 1e-8,
+                 rand_min: float = 0, rand_max: float = 1):
+        Conjugate.__init__(self, f, d, max_iter, tol, h, rand_min, rand_max)
+
+
+class ConjugateSGD(Conjugate, Stochastic):
+    def __init__(self, f: callable, d: int, max_iter: int = 10 ** 5, tol: float = 1e-8, h: float = 1e-8,
+                 rand_min: float = 0, rand_max: float = 1):
+        Conjugate.__init__(self, f, d, max_iter, tol, h, rand_min, rand_max)
+        Stochastic.__init__(self, d, tol)
+
+
+class MiniBatchConjugateGD(Conjugate, MiniBatch):
+    def __init__(self, f: callable, d: int, batch_size: int, max_iter: int = 10 ** 5, tol: float = 1e-8,
+                 h: float = 1e-8, rand_min: float = 0, rand_max: float = 1):
+        Conjugate.__init__(self, f, d, max_iter, tol, h, rand_min, rand_max)
+        MiniBatch.__init__(self, d, batch_size, tol)
